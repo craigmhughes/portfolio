b4f3d2a658425f3aa97e6e850cb105b6aee25b2e
Craig Hughes <hghscraig@gmail.com>
2020-03-24 21:17:20 +0000

    Updates

---
+++ precache-manifest.7f6681fdfe25ba6df3185f1df837694e.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a009ec0b7d4911646d14020364eac50d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3878bbe536c8349bf4a0",
+    "url": "/portfolio/static/css/main.d7ebf7c2.chunk.css"
+  },
+  {
+    "revision": "a097728f3fe5fecf82fb",
+    "url": "/portfolio/static/js/2.8f9e93db.chunk.js"
+  },
+  {
+    "revision": "a91d49ec13c8a97e250979c3fa82a38d",
+    "url": "/portfolio/static/js/2.8f9e93db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3878bbe536c8349bf4a0",
+    "url": "/portfolio/static/js/main.0bde0656.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "7f8228ea65e6c9c79375cc3c17a73548",
+    "url": "/portfolio/static/media/bash.7f8228ea.svg"
+  },
+  {
+    "revision": "b3c7aba4541792a839e0a1d4b848c3a0",
+    "url": "/portfolio/static/media/bass_1.b3c7aba4.wav"
+  },
+  {
+    "revision": "54088c78316e27a8111fcccc11428366",
+    "url": "/portfolio/static/media/bass_2.54088c78.wav"
+  },
+  {
+    "revision": "991f9136e18dffc34557c483a6a9cd10",
+    "url": "/portfolio/static/media/bass_3.991f9136.wav"
+  },
+  {
+    "revision": "0bd6b4db3cbfe7a6aa1717ccfc17b349",
+    "url": "/portfolio/static/media/beat-pad.0bd6b4db.gltf"
+  },
+  {
+    "revision": "8ecdf6cf8931edf3276ad5e391dc8663",
+    "url": "/portfolio/static/media/clap_1.8ecdf6cf.wav"
+  },
+  {
+    "revision": "44afb4c33a336beb7b19a107523c8206",
+    "url": "/portfolio/static/media/clap_2.44afb4c3.wav"
+  },
+  {
+    "revision": "bb2660ee00049c543546e12ffa4f7d0c",
+    "url": "/portfolio/static/media/cv.bb2660ee.pdf"
+  },
+  {
+    "revision": "f6ec2fc30ed2f03892be9e150098b43b",
+    "url": "/portfolio/static/media/hat_1.f6ec2fc3.wav"
+  },
+  {
+    "revision": "ef34d1bebe3cdc99b6a7dadfe7324bdb",
+    "url": "/portfolio/static/media/hat_2.ef34d1be.wav"
+  },
+  {
+    "revision": "d47400cea2a1707fbaba2c87f388f146",
+    "url": "/portfolio/static/media/kick_1.d47400ce.wav"
+  },
+  {
+    "revision": "b15c57f113368f5e5484e466718e5f37",
+    "url": "/portfolio/static/media/kick_2.b15c57f1.wav"
+  },
+  {
+    "revision": "bea042bcda99b786241d4a1e9d80fa87",
+    "url": "/portfolio/static/media/laravel.bea042bc.svg"
+  },
+  {
+    "revision": "2fe917a81ac029a763960957323f800f",
+    "url": "/portfolio/static/media/me.2fe917a8.webp"
+  },
+  {
+    "revision": "699dc788156ec8f8c5602e43f9fcad7a",
+    "url": "/portfolio/static/media/precussion_1.699dc788.wav"
+  },
+  {
+    "revision": "fcab03d7497a0a8aa14a9b31e86654b0",
+    "url": "/portfolio/static/media/precussion_2.fcab03d7.wav"
+  },
+  {
+    "revision": "3fa87670c5c510bf4cd53183c7872d23",
+    "url": "/portfolio/static/media/precussion_3.3fa87670.wav"
+  },
+  {
+    "revision": "146716bdf1a351db5705ec1e6a25ed08",
+    "url": "/portfolio/static/media/python.146716bd.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/portfolio/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "70ddb1882fd7a2f1bd6acd2253f153d2",
+    "url": "/portfolio/static/media/ride_1.70ddb188.wav"
+  },
+  {
+    "revision": "785da4a4f77cbabcfec568bc864bcf91",
+    "url": "/portfolio/static/media/snap_1.785da4a4.wav"
+  },
+  {
+    "revision": "62a24714d154202daa2f639ce34a3fe0",
+    "url": "/portfolio/static/media/snap_2.62a24714.wav"
+  },
+  {
+    "revision": "ba9f79354f7e77c8f146c80e5eb4bfdd",
+    "url": "/portfolio/static/media/snare_1.ba9f7935.wav"
+  },
+  {
+    "revision": "61ed8b054178d0e8d1285ad8f29f4045",
+    "url": "/portfolio/static/media/vanilla-js.61ed8b05.svg"
+  },
+  {
+    "revision": "60dfaadbbd65a730821eb0e7b5974020",
+    "url": "/portfolio/static/media/vue.60dfaadb.svg"
+  }
+]);
